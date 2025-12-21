55ef36901d99a114c45839d0c6d2505af22ed3d3
Idle del <magarkiran436@gmail.com>
2025-12-21 19:37:39 +0545

    allowed space in username and checked if email already exists

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -2,11 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=150, required=True, allow_blank=True, validators=[])
+    email = serializers.EmailField(required=True)
+    
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = ['id']
+        
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
         
     def create(self, validated_data):
         user = User.objects.create_user(
